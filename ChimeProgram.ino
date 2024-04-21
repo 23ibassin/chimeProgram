f716d45b4a9f1edd8963190c8fa14a45e9a5c9c3
23ibassin <58051667+23ibassin@users.noreply.github.com>
2024-04-21 15:08:06 -0400

    Update ChimeProgram.ino

--- ChimeProgram.ino
+++ ChimeProgram.ino
@@ -7,14 +7,14 @@ const int motor2s = 11;
 const int motor2d = 13;
 
 int motorSpd1 = 40;
-int motorSpd2 = 255;           //proportionally same speed as motor 1? NO
-const int motorTime1a = 70;    //C6-A5 to A6-G6
-const int motorTime1b = 80;    //A5-C#6 to A6-G6
-const int motorTime1ab = 125;  //C6-A5 to A5-C#6
+int motorSpd2 = 255;             //proportionally same speed as motor 1? NO
+const int motorTime1a = 70;      //C6-A5 to A6-G6
+const int motorTime1b = 80;  //A5-C#6 to A6-G6
+const int motorTime1ab = 125;    //C6-A5 to A5-C#6
 
-const int motorTime2a;            //E6-F6 to Bb6-C#7
+const int motorTime2a = 0;        //E6-F6 to Bb6-C#7
 const int motorTimeJump2 = 237;   //F6-D6 to E6-F6
-const int motorTimeJump2R = 278;  //E6-F6 to F6-D6
+const int motorTimeJump2R = 282;  //E6-F6 to F6-D6
 
 const int solenoid1 = 6;
 const int solenoid2 = 7;
@@ -116,10 +116,10 @@ void loop() {
       digitalWrite(motor1s, motorSpd1);
 
       digitalWrite(solenoid2, HIGH);  //E6
-      delay(motorTime1ab);
+      delay(motorTime1ab-5);
       digitalWrite(motor1s, 0);
 
-      delay(timing[0] - motorTime1ab);
+      delay(timing[0] - motorTime1ab-5);
       digitalWrite(solenoid2, LOW);
 
     } else {
@@ -144,10 +144,10 @@ void loop() {
   digitalWrite(solenoid3, HIGH);
   delay(timing[0]);
   digitalWrite(solenoid3, LOW);
-
+  // not accurate
   digitalWrite(motor1s, motorSpd1);
   digitalWrite(motor1d, 1);
-  delay(motorTime1b);
+  delay(motorTime1b  + 3);
   digitalWrite(motor1s, 0);
 
   digitalWrite(motor2s, motorSpd2);
@@ -159,23 +159,23 @@ void loop() {
   delay(motorTimeJump2R);
   digitalWrite(motor2s, 0);
 
-  //testing for now
+  //good place to use switch?
   //delay(999999999);
 
-  delay(timing[1] - motorTimeJump2R);
+  delay(timing[1] - motorTimeJump2R+8);
 
-  Serial.println("measure 5");   //measure 5
-  digitalWrite(solenoid1, HIGH); //D6
+  Serial.println("measure 5");    //measure 5
+  digitalWrite(solenoid1, HIGH);  //D6
   delay(timing[0]);
   digitalWrite(solenoid1, LOW);
 
   digitalWrite(motor2s, motorSpd2);
   digitalWrite(motor2d, 0);
-  delay(motorTimeJump2);
+  delay(motorTimeJump2-6);
   digitalWrite(motor2s, 0);
-  delay(timing[4] - motorTimeJump2 - timing[0]);
+  delay(timing[4] - motorTimeJump2-6 - timing[0]);
 
-  digitalWrite(solenoid2, HIGH); //E6
+  digitalWrite(solenoid2, HIGH);  //E6
   delay(timing[1]);
   digitalWrite(solenoid2, LOW);
 
@@ -209,7 +209,7 @@ void loop() {
 
   digitalWrite(motor2s, motorSpd2);
   digitalWrite(motor2d, 1);
-  delay(motorTimeJump2R + 8);
+  delay(motorTimeJump2R + 9);
   digitalWrite(motor2s, 0);
 
   Serial.println("measure 9");
@@ -219,16 +219,16 @@ void loop() {
 
   digitalWrite(motor2s, motorSpd2);
   digitalWrite(motor2d, 0);
-  delay(motorTimeJump2);
+  delay(motorTimeJump2-2);
   digitalWrite(motor2s, 0);
-  delay(timing[4] - motorTimeJump2 - timing[0]);
+  delay(timing[4] - motorTimeJump2-2 - timing[0]);
 
   digitalWrite(solenoid2, HIGH);
   delay(timing[1]);
   digitalWrite(solenoid2, LOW);
 
-  Serial.println("measure 10"); //measure 10
-  digitalWrite(solenoid1, HIGH); 
+  Serial.println("measure 10");  //measure 10
+  digitalWrite(solenoid1, HIGH);
   delay(timing[3]);
   digitalWrite(solenoid1, LOW);
   digitalWrite(solenoid2, HIGH);
@@ -239,35 +239,113 @@ void loop() {
   delay(motorTime1b);
   digitalWrite(motor1s, 0);
 
-  digitalWrite(solenoid3, HIGH); //G6
+  digitalWrite(solenoid3, HIGH);  //G6
   delay(timing[3]);
   digitalWrite(solenoid3, LOW);
-  digitalWrite(solenoid4, HIGH);//A6
+  digitalWrite(solenoid4, HIGH);  //A6
   delay(timing[3]);
   digitalWrite(solenoid4, LOW);
-  digitalWrite(solenoid3, HIGH); //G6
+  digitalWrite(solenoid3, HIGH);  //G6
   delay(timing[3]);
   digitalWrite(solenoid3, LOW);
-  digitalWrite(solenoid2, HIGH); //E6
+  digitalWrite(solenoid2, HIGH);  //E6
   delay(timing[3]);
   digitalWrite(solenoid2, LOW);
 
-
-  Serial.println("measure 11"); //measure 11
-  digitalWrite(solenoid3, HIGH); //G6
-  delay(timing[3]);
-  //digitalWrite(solenoid3, LOW);
+  Serial.println("measure 11");   //measure 11
+  digitalWrite(solenoid3, HIGH);  //G6
+  delay(timing[3] - motorTime1a + 8 - 20);
+  digitalWrite(solenoid3, LOW);
   //going to C6
   digitalWrite(motor1d, 1);
   digitalWrite(motor1s, 1);
-  delay(motorTime1a+1);
+  delay(motorTime1a + 8);
   digitalWrite(motor1s, 0);
-  digitalWrite(solenoid4, HIGH);//C6
-  delay(timing[3]);
+  delay(20);
+  digitalWrite(solenoid4, HIGH);  //C6
+  delay(timing[3] - motorTime1a + 1 - 20);
   digitalWrite(solenoid4, LOW);
- 
-  Serial.println("measure 12"); //measure 12
+
+  Serial.println("measure 12");  //measure 12
+  //going to G6
+  digitalWrite(motor1d, 0);
+  digitalWrite(motor1s, 1);
+  delay(motorTime1a -5);
+  digitalWrite(motor1s, 0);
+  delay(20);
+  digitalWrite(solenoid3, HIGH);  //C6
+  delay(timing[3]);
+  digitalWrite(solenoid3, LOW);
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[3]);
+  digitalWrite(solenoid1, LOW);
+
+  Serial.println("measure 13");  //measure 13
+  delay(timing[1]);              // quarter rest
+  for (int i = 0; i < 3; i++) {
+    //Serial.println(i);
+    digitalWrite(solenoid1, HIGH);  //F6
+    delay(timing[1] - 35);
+    digitalWrite(solenoid1, LOW);
+    delay(35);
+  }
+
+  Serial.println("measure 14");   //measure 14
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid4, LOW);
+  delay(35);
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1]);
+  digitalWrite(solenoid4, LOW);
+  digitalWrite(solenoid3, HIGH);  //G6
+  delay(timing[1]);
+  digitalWrite(solenoid3, LOW);
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[1]);
+  digitalWrite(solenoid1, LOW);
+
+  Serial.println("measure 15");   //measure 15
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid4, LOW);
+  delay(35);
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid4, LOW);
+  delay(35);
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1]);
+  digitalWrite(solenoid4, LOW);
+
+  Serial.println("measure 16");   //measure 16
+  digitalWrite(solenoid3, HIGH);  //G6
+  delay(timing[1]);
+  digitalWrite(solenoid3, LOW);
+  digitalWrite(solenoid4, HIGH);  //A6
+  delay(timing[1]);
+  digitalWrite(solenoid4, LOW);
+  digitalWrite(solenoid3, HIGH);  //G6
+  delay(timing[1]);
+  digitalWrite(solenoid3, LOW);
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[1]);
+  digitalWrite(solenoid1, LOW);
+
+  Serial.println("measure 17");   //measure 17
+  delay(timing[1]);               // quarter rest
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid1, LOW);
+  delay(35);
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid1, LOW);
+  delay(35);
+  digitalWrite(solenoid1, HIGH);  //F6
+  delay(timing[1] - 35);
+  digitalWrite(solenoid1, LOW);
+  delay(35);
+  Serial.println("finished");
   delay(999999999);
-
 }
-
